a54f324dca073b520c46ea7983f993733c5503ad
Patrick Koller <patrick.koller@lhep.unibe.ch>
2020-07-06 18:13:30 +0200

    Patrick: removed files not needed for LCM.

--- ArgonCube/runLCM.sh
+++ ArgonCube/runLCM.sh
@@ -1,2 +1,2 @@
-gegede-cli ../duneggd/Config/ArgonCube/OptSim.cfg ../duneggd/Config/ArgonCube/DETENCLOSURE_LCM.cfg ../duneggd/Config/WORLDggd.cfg -w World -o LCM.gdml
+gegede-cli ../duneggd/Config/ArgonCube/LCM.cfg ../duneggd/Config/ArgonCube/DETENCLOSURE_LCM.cfg ../duneggd/Config/WORLDggd.cfg -w World -o LCM.gdml
 root -l 'materialDisplay.C("LCM.gdml")'
